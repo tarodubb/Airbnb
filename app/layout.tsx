aef73910d1019014fa9352893323684b8e0dfe6a
maxwell wong <midorikawamaxwell@gmail.com>
2023-04-13 20:04:20 +0900

    added bg-neutral overlay for modal and made modal white with an X button

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from 'next/font/google';
 import './globals.css'
 import Navbar from './components/Navbar/Navbar';
 import ClientOnly from './components/ClientOnly';
+import Modal from './components/Modals/Modal';
 
 export const metadata = {
   title: 'Airbnb',
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <Modal isOpen/>
           <Navbar />
         </ClientOnly>
         {children}
