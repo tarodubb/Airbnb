52231ac553c511d75a5e99677e976feb991876b4
maxwell wong <midorikawamaxwell@gmail.com>
2023-04-13 21:04:50 +0900

    installed axios and react-hook-form

---
+++ app/components/Modals/RegisterModal.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import axios from 'axios';
+import { AiFillGithub } from 'react-icons/ai';
+import { FcGoogle } from 'react-icons/fc';
+import { useCallback, useState } from 'react';
+import {
+  FieldValue,
+  SubmitHandler,
+  useForm
+} from 'react-hook-form';
+
+import userRegisterModal from '../../hooks/useRegisterModal';
+
+const RegisterModal = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default RegisterModal;
