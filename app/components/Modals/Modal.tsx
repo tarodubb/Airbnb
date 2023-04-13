729ef2962318afcdf21f3eb2782e08f617a9f151
maxwell wong <midorikawamaxwell@gmail.com>
2023-04-13 20:27:21 +0900

    stylized button so it can be customized to different shapes, colors, and outlines

--- app/components/Modals/Modal.tsx
+++ app/components/Modals/Modal.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState, useCallback } from 'react';
 import { IoMdClose } from 'react-icons/io';
-
+import Button from '../Button';
 interface ModalProps {
   isOpen?: boolean;
   onClose: () => void;
@@ -145,8 +145,7 @@ const Modal: React.FC<ModalProps> = ({
                     w-full
                   '
                 >
-
-
+                  <Button label='My Button'/>
                 </div>
               </div>
             </div>
