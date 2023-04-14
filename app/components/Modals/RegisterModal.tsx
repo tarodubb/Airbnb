12bb6ceadc81bcb7ba83fdf066a357dd861fc6a7
maxwell wong <midorikawamaxwell@gmail.com>
2023-04-14 11:53:38 +0900

    added an open function so that the register modal is triggered when the signup button is clicked on in the usermenu component

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -5,18 +5,56 @@ import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
 import { useCallback, useState } from 'react';
 import {
-  FieldValue,
+  FieldValues,
   SubmitHandler,
   useForm
 } from 'react-hook-form';
 
-import userRegisterModal from '../../hooks/useRegisterModal';
+import useRegisterModal from '../../hooks/useRegisterModal';
+import Modal from './Modal';
 
 const RegisterModal = () => {
-  return (
-    <div>
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
 
-    </div>
+  const {
+    register,
+    handleSubmit,
+    formState: {
+      errors,
+    }
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: ''
+    }
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios.post('./api/register', data)
+      .then(() => {
+        registerModal.onClose();
+      })
+      .catch((error) =>{
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      })
+  }
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+        title='Register'
+        actionLabel='Continue'
+        onClose={registerModal.onClose}
+        onSubmit={handleSubmit(onSubmit)}
+    />
   );
 };
 
